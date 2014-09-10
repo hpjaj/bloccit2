26fdf69868a6b6371ea994032561939c8f7d30f2
hpjaj <hmlevine@gmail.com>
2014-09-10 15:51:31 -0600

    Completed the comments checkpoint
    Got the comments working.  Had difficulties understanding the logic for
    posts_controller/show action and the comments_controller/create action.
    Also line 4 of the comments/_form.html.erb file is not crystal clear.
    Added the validations to the comment model, but did not created a
    dedicated way for those errors to display so the user knows what they
    did wrong.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,16 +3,17 @@ class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
     @comment = Comment.new
-    authorize @comment
+    # authorize @comment
   end
 
   def create
     @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = current_user.comments.new(comment_params)
     @comment.post = @post
-    authorize @comment
+    # authorize @comment
     if @comment.save
-      redirect_to @comment, notice: "Comment was saved successfully."
+      redirect_to [@topic, @post], notice: "Comment was saved successfully."
     else
       flash[:error] = "Error creating comment.  Please try again."
       render :new
@@ -22,19 +23,19 @@ class CommentsController < ApplicationController
   def show
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
-    authorize @comment
+    # authorize @comment
   end
 
   def edit
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
-    authorize @comment
+    # authorize @comment
   end
 
   def update
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
-    authorize @comment
+    # authorize @comment
     if @comment.update_attributes(comment_params)
       
     end
