46ab62dac0468f66d48549248a367662b4a45e58
hpjaj <hmlevine@gmail.com>
2014-09-12 17:41:58 -0600

    Completed the voting checkpoint including first round of rspec testing

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments, only: [:create]
+
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
   
   get 'about' => 'welcome#about'
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,8 +1,17 @@
+require 'spec_helper'
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( Vote.value ).to eq(1) or eq(-1)
+        v = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
+
+        v2 = Vote.new(value: -1)
+        expect(v.valid?).to eq(true)
+
+        bad_v = Vote.new(value: 2)
+        expect(bad_v.valid?).to eq(false)
       end
     end
   end
