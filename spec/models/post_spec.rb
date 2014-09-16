d6fdaafd5fe5e29962d03bcd7baf95c65882c527
hpjaj <hmlevine@gmail.com>
2014-09-15 21:16:47 -0600

    Completed the Mocking with RSpec checkpoint

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,26 +1,10 @@
 require 'rails_helper'
 
-def associated_post(options={})
-  post_options = { 
-   title: 'Post title', 
-   body: 'Post bodies must be pretty long.', 
-   topic: Topic.create(name: 'Topic name'), 
-   user: authenticated_user 
-  }.merge(options)
-  Post.create(post_options)
-end
-
-def authenticated_user(options={})
-  user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-  user = User.new(user_options)
-  user.skip_confirmation!
-  user.save
-  user
-end
-
 describe Post do
-  describe "vote methods" do
 
+  include TestFactories
+  
+  describe "vote methods" do
     before do
       @post = associated_post
       3.times { @post.votes.create(value: 1) }
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Vote do
+
+  include TestFactories
+
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
@@ -21,26 +24,8 @@ describe Vote do
       post = associated_post
       vote = Vote.new(value: 1, post: post)
       expect(post).to receive(:update_rank)
+      vote.save
     end
   end
 
-end
-
-
-def associated_post(options={})
-  post_options = { 
-   title: 'Post title', 
-   body: 'Post bodies must be pretty long.', 
-   topic: Topic.create(name: 'Topic name'), 
-   user: authenticated_user 
-  }.merge(options)
-  Post.create(post_options)
-end
-
-def authenticated_user(options={})
-  user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-  user = User.new(user_options)
-  user.skip_confirmation!
-  user.save
-  user
 end
