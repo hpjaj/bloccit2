bc86280ee2b774b4f358561a8b39f3e8873fcfb3
hpjaj <hmlevine@gmail.com>
2014-09-18 17:05:10 -0600

    Fixed route from a non-logged in user, to a public post

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
  
-  resources :posts, only: [:show] do
+  resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
       resources :favorites, only: [:create, :destroy]
 
